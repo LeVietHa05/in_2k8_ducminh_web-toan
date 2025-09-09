72fab30cef5578e10c5d5ee1a919d7f60ffb9dc2
LeVietHa05 <ha.leviet.0504@gmail.com>
2025-09-09 16:17:35 +0700

    Refactor styles for improved responsiveness and consistency in the Home component

--- app/page.tsx
+++ app/page.tsx
@@ -17,12 +17,12 @@ export default function Home() {
       <div className="max-w-5xl h-1/2 text-[#395FAF]  absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <Title
           text="Tổng hợp các chủ đề toán quan trọng trong chương trình THPT"
-          className="text-5xl leading-16 mb-3"
+          className="2xl:text-5xl text-4xl leading-16 mb-3"
         />
-        <div className="custom-box relative flex gap-12 flex-wrap justify-center bg-[#395FAF] py-20">
+        <div className="custom-box relative flex 2xl:gap-10 xl:gap-6 gap-2 flex-wrap justify-center bg-[#395FAF] 2xl:py-20 lg:py-10 py-4">
           {links.map(link => {
             return (
-              <Link key={link.name} href={link.href} className="min-w-100 p-4 text-4xl bg-[#FEF5CE] rounded-full text-center flex items-center justify-center hover:scale-105">
+              <Link key={link.name} href={link.href} className="min-w-100 p-4 2xl:text-4xl text-3xl bg-[#FEF5CE] rounded-full text-center flex items-center justify-center hover:scale-105">
                 <Title text={link.name} ></Title>
               </Link>
             )
@@ -30,7 +30,7 @@ export default function Home() {
         </div>
       </div>
 
-      <Link href={'/luyentap'} className="absolute bottom-10 left-1/2 -translate-x-1/2 bg-[#FEF5CE] rounded-full px-6 py-4 hover:scale-105">
+      <Link href={'/luyentap'} className="absolute 2xl:bottom-10 lg:bottom-4 bottom-2 left-1/2 -translate-x-1/2 bg-[#FEF5CE] rounded-full px-6 py-4 hover:scale-105">
         <Title text="Luyện tập" className="text-[#7EBF57] text-4xl"></Title>
       </Link>
 
